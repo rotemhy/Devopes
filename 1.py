cb266eeb5ad0488a7fd2a7c62791a0854ec4484b
rotemhy <50592753+rotemhy@users.noreply.github.com>
2020-08-04 19:04:12 +0300

    Changed script

--- 1.py
+++ 1.py
@@ -1,19 +1,4 @@
 # Duplicate machine - The purpose of this script is to copy the data from a file to an empty file
 # Added line
-import math
 
-FILENAME1 = r'Exercise.txt'
-FILENAME2 = r'Solution.txt'
-
-sol_file = open(FILENAME2, 'a')
-
-with open(FILENAME1, 'r') as ex_file:
-    for line in ex_file:
-        line = line.split(" ")
-        line.append('=')
-        
-        if line[1] == '+':
-            line.append(line[0]+line[1])
-        line = ' '.join(line)
-        sol_file.write(line)
-sol_file.close()
+print "Done"
